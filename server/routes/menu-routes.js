b2627a583645a3d5988c1a5a1383bf0ad6d94020
Cameron Sharp <cameron.sharp1980@gmail.com>
2017-11-15 20:18:19 -0700

    Added get route to return all collection items with name and price

--- server/routes/menu-routes.js
+++ server/routes/menu-routes.js
@@ -19,13 +19,31 @@ router.get('/api/menus', (req, res, next) => {
     Burgers.find({})
         .then(burgers => {
             menuItems.burgers = burgers
+            for (let i = 0; i < menuItems.burgers.length; i++) {
+                const burger = menuItems.burgers[i];
+                shortMenu.burgers[i] = {}
+                shortMenu.burgers[i].name = burger.name
+                shortMenu.burgers[i].price = burger.price
+            }
             Drinks.find({})
                 .then(drinks => {
                     menuItems.drinks = drinks
+                    for (let i = 0; i < menuItems.drinks.length; i++) {
+                        const drink = menuItems.drinks[i];
+                        shortMenu.drinks[i] = {}
+                        shortMenu.drinks[i].name = drink.name
+                        shortMenu.drinks[i].prices = drink.sizes
+                    }
                     Sides.find({})
                         .then(sides => {
                             menuItems.sides = sides
-                            res.send(menuItems)
+                            for (let i = 0; i < menuItems.sides.length; i++) {
+                                const side = menuItems.sides[i];
+                                shortMenu.sides[i] = {}
+                                shortMenu.sides[i].name = side.name
+                                shortMenu.sides[i].price = side.price
+                            }
+                            res.send(shortMenu)
                         })
                         .catch(err => {
                             res.status(400).send({ Error: err, message: "sides" })
